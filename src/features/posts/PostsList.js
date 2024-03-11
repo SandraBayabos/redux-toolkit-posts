24503d34cba0e622f16c8b1e8ce03645d203a349
sandrabayabos <sandra.bayabos@gmail.com>
2024-03-11 15:50:39 +0800

    added timestamp to posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { selectAllPosts } from "./postsSlice";
 import React from "react";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
 
 const PostsList = () => {
   const posts = useSelector(selectAllPosts);
@@ -12,6 +13,7 @@ const PostsList = () => {
       <p className="post-content">{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
       </p>
     </article>
   ));
