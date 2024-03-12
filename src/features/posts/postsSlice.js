0ac8d827ebdbb0874ab5c79d1f1403243a891fce
sandrabayabos <sandra.bayabos@gmail.com>
2024-03-12 10:47:15 +0800

    edit code

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -6,18 +6,22 @@ const POSTS_URL = "https://jsonplaceholder.typicode.com/posts";
 
 const initialState = {
   posts: [],
-  status: "idle", // "idle" | "loading" | "succeeded" | "failed"
+  status: "idle", //'idle' | 'loading' | 'succeeded' | 'failed'
   error: null,
 };
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
-  try {
-    const response = await axios.get(POSTS_URL);
-    return [...response.data];
-  } catch (error) {
-    return error.message;
-  }
+  const response = await axios.get(POSTS_URL);
+  return [...response.data]
 });
+
+export const addNewPost = createAsyncThunk(
+  "posts/addNewPost",
+  async (initialPost) => {
+    const response = await axios.post(POSTS_URL, initialPost);
+    return response.data;
+  }
+);
 
 const postsSlice = createSlice({
   name: "posts",
@@ -53,35 +57,60 @@ const postsSlice = createSlice({
         existingPost.reactions[reaction]++;
       }
     },
-    extraReducers(builder) {
-      builder
-        .addCase(fetchPosts.pending, (state, action) => {
-          state.status = "loading";
-        })
-        .addCase(fetchPosts.fulfilled, (state, action) => {
-          state.status = "succeeded";
-          // Adding date and reactions
-          let min = 1;
-          const loadedPosts = action.payload.map((post) => {
-            post.date = sub(new Date(), { minutes: min++ }).toISOString();
-            post.reactions = {
-              thumbsUp: 0,
-              wow: 0,
-              heart: 0,
-              rocket: 0,
-              coffee: 0,
-            };
-            return post;
-          });
-
-          // Add any fetched posts to the array
-          state.posts = state.posts.concat(loadedPosts);
-        })
-        .addCase(fetchPosts.rejected, (state, action) => {
-          state.status = "failed";
-          state.error = action.error.message;
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPosts.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        // Adding date and reactions
+        let min = 1;
+        const loadedPosts = action.payload.map((post) => {
+          post.date = sub(new Date(), { minutes: min++ }).toISOString();
+          post.reactions = {
+            thumbsUp: 0,
+            wow: 0,
+            heart: 0,
+            rocket: 0,
+            coffee: 0,
+          };
+          return post;
         });
-    },
+
+        // Add any fetched posts to the array
+        state.posts = state.posts.concat(loadedPosts);
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      })
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        // Fix for API post IDs:
+        // Creating sortedPosts & assigning the id
+        // would be not be needed if the fake API
+        // returned accurate new post IDs
+        const sortedPosts = state.posts.sort((a, b) => {
+          if (a.id > b.id) return 1;
+          if (a.id < b.id) return -1;
+          return 0;
+        });
+        action.payload.id = sortedPosts[sortedPosts.length - 1].id + 1;
+        // End fix for fake API post IDs
+
+        action.payload.userId = Number(action.payload.userId);
+        action.payload.date = new Date().toISOString();
+        action.payload.reactions = {
+          thumbsUp: 0,
+          hooray: 0,
+          heart: 0,
+          rocket: 0,
+          eyes: 0,
+        };
+        console.log(action.payload);
+        state.posts.push(action.payload);
+      });
   },
 });
 
