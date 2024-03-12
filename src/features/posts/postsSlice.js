94c5ba7608aa12ed7e3a0e675606fa89635bd206
sandrabayabos <sandra.bayabos@gmail.com>
2024-03-12 12:22:51 +0800

    added routing

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -105,9 +105,13 @@ const postsSlice = createSlice({
   },
 });
 
+// selectors
 export const selectAllPosts = (state) => state.posts.posts;
 export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
+
+export const selectPostById = (state, postId) =>
+  state.posts.posts.find((post) => post.id === postId);
 
 export const { postAdded, reactionAdded, extraReducers } = postsSlice.actions;
 
