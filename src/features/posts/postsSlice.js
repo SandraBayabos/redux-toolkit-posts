5519cb7104d05983f6ffe64b9a87b1359baad03c
sandrabayabos <sandra.bayabos@gmail.com>
2024-03-12 14:41:52 +0800

    delete post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -12,7 +12,7 @@ const initialState = {
 
 export const fetchPosts = createAsyncThunk("posts/fetchPosts", async () => {
   const response = await axios.get(POSTS_URL);
-  console.log(response.data)
+  console.log(response.data);
   return response.data;
 });
 
@@ -22,6 +22,33 @@ export const addNewPost = createAsyncThunk(
     try {
       const response = await axios.post(POSTS_URL, initialPost);
       return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
+
+export const updatePost = createAsyncThunk(
+  "posts/updatePost",
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.put(`${POSTS_URL}/${id}`, initialPost);
+      return response.data;
+    } catch (err) {
+      return err.message;
+    }
+  }
+);
+
+export const deletePost = createAsyncThunk(
+  "posts/deletePost",
+  async (initialPost) => {
+    const { id } = initialPost;
+    try {
+      const response = await axios.delete(`${POSTS_URL}/${id}`);
+      if (response?.status === 200) return initialPost;
+      return `${response?.status}: ${response?.statusText}`;
     } catch (err) {
       return err.message;
     }
@@ -102,6 +129,26 @@ const postsSlice = createSlice({
           eyes: 0,
         };
         state.posts.push(action.payload);
+      })
+      .addCase(updatePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Update could not complete");
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        action.payload.date = new Date().toISOString();
+        const posts = state.posts.filter((post) => post.id !== id);
+        state.posts = [...posts, action.payload];
+      })
+      .addCase(deletePost.fulfilled, (state, action) => {
+        if (!action.payload?.id) {
+          console.log("Delete could not complete");
+          console.log(action.payload);
+          return;
+        }
+        const { id } = action.payload;
+        state.posts = state.posts.filter((post) => post.id !== id);
       });
   },
 });
@@ -112,7 +159,7 @@ export const getPostsStatus = (state) => state.posts.status;
 export const getPostsError = (state) => state.posts.error;
 
 export const selectPostById = (state, postId) =>
-  state.posts.posts.find(post => post.id === postId);
+  state.posts.posts.find((post) => post.id === postId);
 
 export const { postAdded, reactionAdded, extraReducers } = postsSlice.actions;
 
